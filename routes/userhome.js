228e80771f46742c336322cb3b1286fb04d13943
MikeLouieBania <111558033+MikeLouieBania@users.noreply.github.com>
2023-05-14 19:46:06 +0800

    Update userhome.js

--- routes/userhome.js
+++ routes/userhome.js
@@ -6,6 +6,10 @@ const prisma = new PrismaClient();
 
 /* GET users listing. */
 router.get('/user', async function(req, res, next) {
+    var users = await prisma.User.findMany()
+    if (!req.session.userId) {
+    return res.redirect('/login');
+  }
   const users = await prisma.user.findMany();
   res.render('user', { title: 'Users', users: users });
 });
