b0632d622af033c24c4f05ce42c774ec0e75c0e9
MikeLouieBania <111558033+MikeLouieBania@users.noreply.github.com>
2023-05-14 20:08:13 +0800

    Update adminhome.js

--- routes/adminhome.js
+++ routes/adminhome.js
@@ -1,25 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const session = require('express-session');
-const app = express();
-app.use(express.json());
 
 const {PrismaClient, Prisma} = require("@prisma/client")
 var prisma = new PrismaClient
 
-app.use(session({
-  secret: 'secret-key',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 86400000 // 24 hours
-  }
-}));
-
 /* GET home page. */
 router.get('/admin', async function(req, res, next) {
   var users = await prisma.User.findMany()
-
+        if (!req.session.userId) {
+            return res.redirect('/login');
+        }
   res.render('admin', { title: 'Express', users: users });
 });
 
