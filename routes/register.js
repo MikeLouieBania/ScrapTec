551f544777ddf484100e3601c3ae1de5d7b6b145
MikeLouieBania <111558033+MikeLouieBania@users.noreply.github.com>
2023-05-14 18:04:45 +0800

    Update register.js

--- routes/register.js
+++ routes/register.js
@@ -1,48 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const validator = require('email-validator');
+const crypto = require('crypto');
 
-const {PrismaClient, Prisma} = require("@prisma/client")
-var prisma = new PrismaClient
-
-function encrypt(text, shift) {
-  let result = '';
-  for (let i = 0; i < text.length; i++) {
-    let char = text.charAt(i);
-    // Check if character is a letter
-    if (/[a-zA-Z]/.test(char)) {
-      let code = text.charCodeAt(i);
-      // Uppercase letters
-      if (code >= 65 && code <= 90) {
-        char = String.fromCharCode(((code - 65 + shift) % 26) + 65);
-      }
-      // Lowercase letters
-      else if (code >= 97 && code <= 122) {
-        char = String.fromCharCode(((code - 97 + shift) % 26) + 97);
-      }
-    }
-    // Check if character is a number or special character
-    else if (/[0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(char)) {
-      let code = text.charCodeAt(i);
-      char = String.fromCharCode((code + shift) % 128);
-    }
-    result += char;
-  }
-  return result;
-}
+const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient();
 
 /* GET home page. */
-router.get('/register', async function(req, res, next) {
-  var users = await prisma.User.findMany()
-
-  res.render('register', { title: 'Express', users: users }); 
+router.get('/register', async (req, res, next) => {
+  const users = await prisma.user.findMany();
+  res.render('register', { title: 'Express', users }); 
 });
 
 router.post('/register', async (req, res) => {
   const { email, password, usertype } = req.body;
 
   if (!validator.validate(email)) {
-    return res.render('register', { message: 'Invalid email address' });
+    return res.render('register', { errorMessage: 'Invalid email address' });
   }
 
   const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/;
@@ -50,16 +24,21 @@ router.post('/register', async (req, res) => {
     return res.render('register', { message: 'Password must be: 8 characters minimum, at least one number, one lowercase letter, one uppercase letter, and one special character' });
   }
 
-  const encryptedPassword = encrypt(password,3);
-  const user = await prisma.User.findUnique({ where: { email } });
+  const salt = crypto.randomBytes(16).toString('hex'); // generate a random salt
+  const hash = crypto.createHash('sha256'); // create a new hash object
+  hash.update(password + salt); // update the hash with the password and salt
+  const hashedPassword = hash.digest('hex'); // get the digest (hashed value) as a hex string
+
+  const user = await prisma.user.findUnique({ where: { email } });
   
   if (user) {
-   return res.render('register', { errorMessage: `Email already exists, please choose a different one`});
+    return res.render('register', { errorMessage: `Email already exists`});
   } else {
-    await prisma.User.create({
+    await prisma.user.create({
       data: {
         email,
-        password: encryptedPassword,
+        password: hashedPassword,
+        salt,
         usertype
       }
     });
