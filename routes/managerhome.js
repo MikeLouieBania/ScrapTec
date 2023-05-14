a61713aa5d761c08b69252cb4d74774ba9c2da01
MikeLouieBania <111558033+MikeLouieBania@users.noreply.github.com>
2023-05-14 21:00:34 +0800

    Update managerhome.js

--- routes/managerhome.js
+++ routes/managerhome.js
@@ -9,7 +9,7 @@ router.get('/manager', async function(req, res, next) {
   var users = await prisma.User.findMany()
         if (!req.session.userId) {
             return res.redirect('/login');
-        } else if (req.session.userId) {
+        } else {
             return res.redirect('/manager');
         }
   res.render('manager', { title: 'Manager', users: users });
