f6e98a6cda30c5afe5703fa8d3fa51843e281f24
MikeLouieBania <111558033+MikeLouieBania@users.noreply.github.com>
2023-05-14 19:30:00 +0800

    Update userhome.js

--- routes/userhome.js
+++ routes/userhome.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const session = require('express-session');
 const app = express();
 app.use(express.json());
