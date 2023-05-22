e6ddfe8e5889c00649059c5ebf71b5b545ef57a9
MikeLouieBania <tamamopasta@gmail.com>
2023-05-22 18:02:28 +0800

    Added Edit on information

--- routes/studentinfo.js
+++ routes/studentinfo.js
@@ -27,7 +27,7 @@ router.post('/studentinfo', async function(req, res) {
         address: req.body.address,
         city: req.body.city,
         region: req.body.region,
-        zipcode:req.body.zipcode,
+        zipcode: req.body.zipcode,
         birthdate: new Date(req.body.birthdate),
         gender: req.body.gender,
         civil_status: req.body.civil_status,
@@ -35,7 +35,7 @@ router.post('/studentinfo', async function(req, res) {
       }
     });
 
-    res.redirect('/studentinfo');
+    res.redirect('/studentDashboard');
   } catch (error) {
     console.error(error);
     res.render('studentinfo', { title: 'Student Information', error: error.message });
@@ -43,7 +43,7 @@ router.post('/studentinfo', async function(req, res) {
 });
 
 // GET route for viewing the student information
-router.get('/studentinfo', async function(req, res) {
+router.get('/viewinfo', async function(req, res) {  // Changed the route to '/viewinfo' to avoid conflict
   try {
     // Check if the user is logged in
     if (!req.session.userId) {
@@ -51,27 +51,49 @@ router.get('/studentinfo', async function(req, res) {
     }
 
     // Fetch the student info for the logged-in user
-    const studentInfo = await prisma.studentInfo.findUnique({
+    const studentInfo = await prisma.student_Info.findUnique({  // Changed 'studentInfo' to 'student_Info' to match the table name
       where: {
         userId: req.session.userId
       }
     });
-    
-    res.render('viewinfo', { title: 'View/Edit Student Information', studentInfo });
+
+    res.render('viewinfo', { title: 'View/Edit Student Information', studentInfo });  // Updated the title
   } catch (error) {
     console.error(error);
-    res.render('viewinfo', { title: 'View/Edit Student Information', error: error.message });
+    res.render('viewinfo', { title: 'View/Edit Student Information', error: error.message });  // Updated the title
   }
 });
 
-// POST route for editing the student info form
-router.post('/studentinfo', async function(req, res, next) {
+// GET route for editing the student info form
+router.get('/editstudentinfo', async function(req, res) {
+  try {
+    // Check if the user is logged in
+    if (!req.session.userId) {
+      return res.redirect('/');
+    }
+
+    // Fetch the student info for the logged-in user
+    const studentInfo = await prisma.student_Info.findUnique({
+      where: {
+        userId: req.session.userId
+      }
+    });
+
+    res.render('editstudentinfo', { title: 'Edit Student Information', studentInfo });
+  } catch (error) {
+    console.error(error);
+    res.render('editstudentinfo', { title: 'Edit Student Information', error: error.message });
+  }
+});
+
+// POST route for updating the student info form
+router.post('/editstudentinfo', async function(req, res, next) {
   try {
     const userId = req.session.userId;
-    const { lastname, firstname, middlename, address, city, region, country, zipcode, birthdate, gender, civil_status, hobby } = req.body;
+    const { lastname, firstname, middlename, address, city, region, zipcode, birthdate, gender, civil_status, hobby } = req.body;
 
     // Update the student info record
-    const studentInfo = await prisma.studentInfo.update({
+    const studentInfo = await prisma.student_Info.update({
       where: { userId },
       data: {
         lastname,
@@ -83,13 +105,13 @@ router.post('/studentinfo', async function(req, res, next) {
         zipcode,
         birthdate: new Date(birthdate),
         gender,
-        civilStatus: civil_status,
+        civil_status,
         hobby: hobby ? hobby : []
       }
     });
 
     console.log(studentInfo);
-    res.redirect('/studentinfo');
+    res.redirect('/studentDashboard');  // Redirect to '/viewinfo' after editing
   } catch (err) {
     console.log(err);
     res.render('error', { errorMessage: 'Error updating student information.' });
