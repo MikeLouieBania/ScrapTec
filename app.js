cea1bdd6bed1a22aabcd075fb653778284154cb7
JaphetRugas <rugas.jadejaphet.123@gmail.com>
2023-09-13 11:08:53 +0800

    asd

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ var adminRoutes = require('./routes/admin');
 
 var app = express();
 
-asdasd
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
