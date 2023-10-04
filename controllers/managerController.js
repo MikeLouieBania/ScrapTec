297532b792a82e75a77921561a78a70f4b638a26
MarielAnnOronos <mmajjoronos@tip.edu.ph>
2023-10-04 16:13:45 +0800

    update manager controller

--- controllers/managerController.js
+++ controllers/managerController.js
@@ -10,24 +10,15 @@ module.exports = {
     try {
       const { email, password } = req.body;
   
-      // Fetch the manager based on the email to get its ID
-      const manager = await prisma.manager.findUnique({
+      
+      // Fetch the drop point associated with the manager's email
+      const dropPoint = await prisma.dropPoint.findFirst({
         where: {
-          email: email
+          manager: {
+            email: email
+          }
         }
-      });
-  
-      // If no manager is found with that email, render the login page with an error
-      if (!manager) {
-        return res.render('manager/login', { error: "Invalid credentials." });
-      }
-  
-      // Fetch the drop point associated with this manager
-      const dropPoint = await prisma.dropPoint.findUnique({
-        where: {
-          managerId: manager.id
-        }
-      });
+      }); 
   
       // If the manager has no associated drop point or the password doesn't match, render the login page with an error
       if (!dropPoint || dropPoint.password !== password) {
@@ -35,7 +26,7 @@ module.exports = {
       }
 
       // Store manager's ID in session for future requests
-      req.session.managerId = manager.id;
+      req.session.managerId = dropPoint.managerId;
   
       // Successful login, redirect to the dashboard.
       res.redirect('/manager/dashboard');
@@ -56,8 +47,17 @@ module.exports = {
         }
       });
   
+      // Check if managerWithDropPoint or managerWithDropPoint.dropPoint is null
+      if (!managerWithDropPoint || !managerWithDropPoint.dropPoint) {
+        return res.status(404).send("Manager or associated drop point not found");
+      }
+  
+      // Extract the drop point name or provide a default value if it's not available  
+      const dropPointName = managerWithDropPoint.dropPoint[0]?.name || "Unnamed Drop Point";
+
+  
       // Render the dashboard with the manager's profile and drop point name
-      res.render('manager/dashboard', { manager: managerWithDropPoint, dropPointName: managerWithDropPoint.dropPoint?.name });
+      res.render('manager/dashboard', { manager: managerWithDropPoint, dropPointName });
   
     } catch (error) {
       console.error("Error fetching manager's profile:", error);
@@ -66,42 +66,93 @@ module.exports = {
   },
   async getManageDonation(req, res) {
     try {
-        // Fetch the manager's profile and associated drop point using the ID stored in the session
-        const managerWithDropPoint = await prisma.manager.findUnique({
-          where: {
-            id: req.session.managerId
-          },
-          include: {
-            dropPoint: {
-              include: {
-                donations: {
-                  where: { isSubmitted: true },  // Fetch only the submitted donations
-                  include: { organization: true }  // Fetch related organization for each donation
-                }
+      // Fetch the manager's profile and associated drop point using the ID stored in the session
+      const managerWithDropPoint = await prisma.manager.findUnique({
+        where: {
+          id: req.session.managerId
+        },
+        include: {
+          dropPoint: {
+            include: {
+              donations: {
+                where: { isSubmitted: true },  // Fetch only the submitted donations
+                include: { 
+                  organization: true,
+                  peripherals: true
+                }  // Fetch related organization for each donation
               }
             }
           }
-        }); 
-        
-        // Format the Expected Date of Arrival for each donation
-        managerWithDropPoint.dropPoint.donations.forEach(donation => {
-          if (donation.expectedDateOfArrival) {
-              const dateObj = new Date(donation.expectedDateOfArrival);
-              donation.formattedDateOfArrival = `${dateObj.getDate()} ${dateObj.toLocaleString('default', { month: 'long' })} ${dateObj.getFullYear()}`;
-          }
+        }
       });
-
-        // Render the manageDonation view, passing in the donations and dropPoint name
-        res.render('manager/manageDonation', { 
-          donations: managerWithDropPoint.dropPoint.donations,
-          dropPointName: managerWithDropPoint.dropPoint.name,  
+  
+      // Get the drop point name from the first drop point (if it exists)
+      const dropPointName = managerWithDropPoint.dropPoint[0]?.name || "Unnamed Drop Point";
+  
+      // Render the manageDonation view, passing in the donations and dropPoint name
+      res.render('manager/manageDonation', { 
+        donations: managerWithDropPoint.dropPoint[0]?.donations || [],
+        dropPointName: dropPointName,
       });
-
+  
     } catch (error) {
-        console.error("Error fetching donations:", error);
-        res.status(500).send("Internal Server Error");
+      console.error("Error fetching donations:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  }, 
+  async updateDonationStatus(req, res) {
+    try {
+      const { donationId, newStatus } = req.body;
+  
+      // Fetch the existing donation
+      const donation = await prisma.donation.findUnique({
+        where: { id: donationId },
+        select: {
+          points: true,
+          organizationId: true
+        }
+      });
+  
+      // Check if the donation exists and has points.
+      if (!donation || donation.points === null) {
+        return res.status(404).send("Donation not found or points are null");
+      }
+    
+      // Update the donation status using donationId and newStatus
+      await prisma.donation.update({
+        where: { id: donationId },
+        data: { status: newStatus },
+      });
+  
+      // Proceed with updating the organization's points if the new status is "VERIFIED" or "ACCEPTEDWITHISSUES"
+      if (newStatus === "VERIFIED" || newStatus === "ACCEPTEDWITHISSUES") {
+        // Fetch the existing organization
+        const organization = await prisma.organization.findUnique({
+          where: { id: donation.organizationId }
+        });
+
+        if (!organization) {
+          return res.status(404).send("Organization not found");
+        }
+
+        const newTotalPoints = (organization.totalPoints || 0) + (donation.points || 0);
+       
+
+        // Update the organization's totalPoints
+        await prisma.organization.update({
+          where: { id: donation.organizationId },
+          data: { totalPoints: newTotalPoints }
+        });
+      }
+  
+      // Redirect back to the donations management page
+      res.redirect('/manager/manageDonation');
+    } catch (error) {
+      console.error("Error updating donation status:", error);
+      res.status(500).send("Internal Server Error");
     }
   },
+    
   async getManagerAccount(req, res) {
     try {
       // Fetch the manager's profile and associated drop point using the ID stored in the session
@@ -113,9 +164,13 @@ module.exports = {
           dropPoint: true  // Fetch associated drop point details
         }
       });
+      
+      // Get the drop point name from the first drop point (if it exists)
+      const dropPointName = managerWithDropPoint.dropPoint[0]?.name || "Unnamed Drop Point";
+
   
       // Render the manager account page with the manager's profile and drop point name
-      res.render('manager/manageraccount', { manager: managerWithDropPoint, dropPointName: managerWithDropPoint.dropPoint?.name });
+      res.render('manager/manageraccount', { manager: managerWithDropPoint, dropPointName: dropPointName });
   
     } catch (error) {
       console.error("Error fetching manager's profile:", error);
