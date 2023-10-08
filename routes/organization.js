b7c1413f6a0a4f6fce7a98c62c0ac13e26942a48
JaphetRugas <rugas.jadejaphet.123@gmail.com>
2023-10-09 01:56:50 +0800

    advertisements

--- routes/organization.js
+++ routes/organization.js
@@ -1,7 +1,17 @@
 const express = require('express'); 
 const multer = require('multer');
+const sharp = require('sharp');
 const router = express.Router();
 const organizationController = require('../controllers/organizationController');
+// Set up Multer storage
+const storage = multer.memoryStorage(); // This will store the image in memory
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024, // Limit to 5MB
+    },
+});
 
 router.get('/dashboard', organizationController.getDashboard); 
 router.post('/submit-feedback', organizationController.submitFeedback);
@@ -14,7 +24,8 @@ router.get('/faq', organizationController.getFAQ);
 router.get('/account', organizationController.getAccount); 
 router.get('/advertisements', organizationController.getAdvertisements); 
 router.get('/spentPoints', organizationController.getSpentPoints);  
-router.get('/advertise/:cityId', organizationController.getAdCity);
+router.get('/advertise/:cityId', organizationController.getAdCity); 
+router.post('/submitAdvertisement', upload.single('imageUrl'), organizationController.submitAdvertisement);
 router.post('/logout', organizationController.logout); 
 
 module.exports = router;
