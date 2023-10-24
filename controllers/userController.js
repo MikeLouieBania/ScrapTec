e1e70deeaa73e66346e4d417b01500d7f1111cd3
JaphetRugas <rugas.jadejaphet.123@gmail.com>
2023-10-24 18:53:25 +0800

    asa

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,10 +17,92 @@ function getMimeType(extension) {
   }
 }
 
-
-
 module.exports = {
   async getMarketplace(req, res) {
+    try {
+      // Get user's city from the session
+      const userCityId = req.session.user.cityId;
+      const currentUserId = req.session.user.id;
+
+      // Fetch N active advertisements for the user's city
+      const advertisements = await prisma.advertisement.findMany({
+        where: {
+          cityId: userCityId,
+          isActive: true,
+          expiryDate: {
+            gte: new Date(),
+          },
+        },
+        include: {
+          organization: true,
+        },
+        take: 10, // You can modify this value depending on how many ads you want to fetch at once
+      });
+
+
+      // Convert each advertisement's imageUrl to Base64.
+      advertisements.forEach(ad => {
+        if (ad.imageUrl) {
+          ad.imageUrl = ad.imageUrl.toString('base64');
+        }
+      });
+
+      // Create a weighted array
+      let weightedAds = [];
+
+      for (let ad of advertisements) {
+        // The multiplication factor determines the weight. 
+        // For example, if an organization has 1000 lifetimePoints, the ad will appear 1000 times.
+        let weight = Math.floor(ad.organization.lifetimePoints);
+
+        // Populate the weightedAds array
+        for (let i = 0; i < weight; i++) {
+          weightedAds.push(ad);
+        }
+      }
+
+      // Randomly select 4 ads (or fewer if not enough distinct ads available) from the weightedAds array
+      let selectedAds = [];
+      for (let i = 0; i < 4 && weightedAds.length > 0; i++) {
+        const randomIndex = Math.floor(Math.random() * weightedAds.length);
+        selectedAds.push(weightedAds[randomIndex]);
+
+        // Remove all occurrences of the selected ad from the weightedAds array to avoid duplicates
+        weightedAds = weightedAds.filter(ad => ad.id !== weightedAds[randomIndex].id);
+      }
+
+
+      // Fetch all listings except for the ones made by the currently logged-in user
+      const listings = await prisma.listing.findMany({
+        where: {
+          NOT: {
+            userId: currentUserId,
+          },
+        },
+        include: {
+          photos: true, // Include photos of the listings
+        },
+      });
+
+      // Convert each listing's photos' imageUrl to Base64. 
+      listings.forEach(listing => {
+        if (listing.photos && listing.photos.length > 0) {
+          listing.photos.forEach(photo => {
+            if (photo.imageUrl) {
+              // Get the MIME type based on the extension (assuming photo has a property 'extension')
+              const mimeType = getMimeType(photo.extension);
+              photo.imageUrl = mimeType + photo.imageUrl.toString('base64');
+            }
+          });
+        }
+      });
+
+      res.render('user/marketplace', { user: req.session.user, advertisement: selectedAds, listings: listings });
+
+    } catch (error) {
+      console.error("Error fetching marketplace:", error);
+      res.status(500).send("Internal Server Error");
+    }
   },
 
   async getListing(req, res) {
@@ -58,8 +140,6 @@ module.exports = {
           ]
         }
       });
-
-
 
       res.render('user/listing', { user: req.session.user, listing: listing, existingConversation: existingConversation });
 
